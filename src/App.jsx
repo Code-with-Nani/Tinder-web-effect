8dc3d06b2d6374c6244439a3c149b5ed5149f2a9
Code-with-Nani <usmanhanan02@gmail.com>
2024-04-23 09:59:44 +0500

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import { FaComment } from "react-icons/fa";
 import { FaArrowUpLong } from "react-icons/fa6";
@@ -22,6 +22,12 @@ import bg1 from "./assets/images/disp1.avif";
 import bg2 from "./assets/images/disp2.avif";
 import bg3 from "./assets/images/displ3.avif";
 import bg4 from "./assets/images/displ4.avif";
+
+import { useGSAP } from "@gsap/react";
+import gsap, { Power4 } from "gsap";
+import { motion } from "framer-motion";
+
+gsap.registerPlugin(useGSAP);
 
 const App = () => {
   const users = [
@@ -96,94 +102,128 @@ const App = () => {
   ];
 
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [isAnimating, setIsAnimating] = useState(false);
 
   const currentObject = users[currentIndex];
 
   const handleClick = () => {
-    const nextIndex = (currentIndex + 1) % users.length;
-    setCurrentIndex(nextIndex);
+    if (!isAnimating) {
+      setIsAnimating(true);
+    }
   };
 
+  useEffect(() => {
+    if (isAnimating) {
+      const nextIndex = (currentIndex + 1) % users.length;
+      setCurrentIndex(nextIndex);
+      setTimeout(() => {
+        setIsAnimating(false);
+      }, 1300);
+    }
+  }, [isAnimating]);
+
+  useGSAP(() => {
+    gsap.from(".bgImg ", {
+      scale: 0.9,
+      opacity: 0,
+      ease: "power4.out",
+      duration: 2,
+    });
+
+    const tl = gsap.timeline();
+    tl.from(".details .part", {
+      y: "100%",
+      opacity: 0,
+      ease: "power4",
+      stagger: 0.06,
+      duration: 1,
+    });
+  }, [currentIndex]);
+
   return (
-    <div className="forMap">
-      <div className="main w-full h-screen">
-        <div className="bgImg w-full h-screen bg-[#FC5149] relative">
-          <img
-            src={currentObject.bgImg}
-            alt=""
-            className="w-full h-full object-cover "
-          />
-        </div>
-        <div className="aboveStructure w-full h-screen  absolute inset-0 flex flex-col justify-between ">
-          <div className="navbar w-full  px-5 py-6 flex justify-between items-center">
-            <div className="prflImg w-[3rem] h-[3rem] bg-red-900 rounded-full overflow-hidden">
-              <img
-                src={currentObject.prflImg}
-                alt=""
-                className="w-full h-full object-cover"
-              />
-            </div>
-            <div className="text text-white text-2xl tracking-tighter font-medium">
-              They liked you.
-            </div>
-            <div className="chatIcon w-[3rem] h-[3rem] bg-white/20 rounded-full flex justify-center items-center relative">
-              <FaComment className="text-white/80" />
-              <div className="badge w-5 h-5 bg-[#FC5149] absolute -top-1 right-0 flex justify-center items-center rounded-full">
-                <h5 className="text-center translate-y-[1px] leading-none font-medium text-xs">
-                  {currentObject.pendingMessage}
-                </h5>
-              </div>
+    <div className="box w-full h-screen bg-black">
+      <div className="bgImg w-full h-screen xl:w-[60%] mx-auto bg-[#FC5149] relative">
+        <img
+          src={currentObject.bgImg}
+          alt=""
+          className="w-full h-full object-cover "
+        />
+      </div>
+      <div className="aboveStructure select-none w-full h-screen  absolute inset-0 flex flex-col justify-between ">
+        <div className="navbar w-full xl:w-[60%] mx-auto px-5 py-6 flex justify-between items-center">
+          <div className="prflImg w-[3rem] h-[3rem] bg-red-900 rounded-full overflow-hidden">
+            <img
+              src={currentObject.prflImg}
+              alt=""
+              className="w-full h-full object-cover"
+            />
+          </div>
+          <div className="text text-white text-2xl tracking-tighter font-medium">
+            They liked you.
+          </div>
+          <div className="chatIcon w-[3rem] h-[3rem] bg-white/20 rounded-full flex justify-center items-center relative">
+            <FaComment className="text-white/80" />
+            <div className="badge w-5 h-5 bg-[#FC5149] absolute -top-1 right-0 flex justify-center items-center rounded-full">
+              <h5 className="text-center translate-y-[1px] leading-none font-medium text-xs">
+                {currentObject.pendingMessage}
+              </h5>
             </div>
           </div>
-          <div className="details w-full px-5 py-6 pb-16 bg-gradient-to-b from-transparent to-black/70">
-            <div className="part-1 location flex items-center gap-4">
-              <div className="icon bg-white rounded-full w-[1.6rem] h-[1.6rem] py-1 flex justify-center items-center">
-                <FaArrowUpLong className=" rotate-[35deg]" />ٖ
-              </div>
-              <h2 className="text-white translate-y-[2px]">
-                {currentObject.location}
-              </h2>
+        </div>
+        <div className="details overflow-hidden h-fit w-full xl:w-[60%] mx-auto  px-5 py-6 pb-16 sm:pb-5  bg-gradient-to-b from-transparent to-black/80">
+          <div className="part overflow-hidden h-fit location flex items-center gap-4">
+            <div className="icon bg-white rounded-full w-[1.6rem] h-[1.6rem] py-1 flex justify-center items-center overflow-hidden select-none">
+              <FaArrowUpLong className="rotate-[40deg]" />
             </div>
-            <div className="part-2 name text-white flex gap-5 mt-2 ">
-              <h1 className="text-7xl tracking-tighter">
-                {currentObject.name}
-              </h1>
-              <h1 className="text-7xl tracking-tighter opacity-40">20</h1>
+            <h2 className="text-white translate-y-[2px] overflow-hidden h-fit">
+              {currentObject.location}
+            </h2>
+          </div>
+          <div className="part overflow-hidden h-fit name text-white flex gap-5 mt-2 ">
+            <h1 className="text-7xl tracking-tighter overflow-hidden h-fit">
+              {currentObject.name}
+            </h1>
+            <h1 className="text-7xl tracking-tighter overflow-hidden h-fit opacity-40">
+              20
+            </h1>
+          </div>
+          <div className="part overflow-hidden h-fit  tags flex gap-2 text-white mt-2">
+            <div className="tag1 overflow-hidden h-fit flex gap-2 items-center bg-white/30 rounded-full px-3 py-1">
+              <span className="text-sm">{currentObject.tag1.icon}</span>
+              <h3 className="text-sm tracking-tight translate-y-[1px]">
+                {currentObject.tag1.text}
+              </h3>
             </div>
-            <div className="part-3 tags flex gap-2 text-white">
-              <div className="tag1 flex gap-2 items-center bg-white/30 rounded-full px-3 py-1">
-                <span className="text-sm">{currentObject.tag1.icon}</span>
-                <h3 className="text-sm tracking-tight translate-y-[1px]">
-                  {currentObject.tag1.text}
-                </h3>
-              </div>
-              <div className="tag2 flex gap-2 items-center bg-white/30 rounded-full px-3 py-1">
-                <span className="text-sm">{currentObject.tag2.icon}</span>
-                <h3 className="text-sm tracking-tight translate-y-[1px]">
-                  {currentObject.tag2.text}
-                </h3>
-              </div>
+            <div className="tag2 overflow-hidden h-fit flex gap-2 items-center bg-white/30 rounded-full px-3 py-1">
+              <span className="text-sm">{currentObject.tag2.icon}</span>
+              <h3 className="text-sm tracking-tight translate-y-[1px]">
+                {currentObject.tag2.text}
+              </h3>
             </div>
-            <div className="part-4 bio ">
-              <h2 className="text-xl mt-4 text-white">Bio</h2>
-              <p className="text-lg tracking-tight mt-1 leading-tight text-white/90">
-                {currentObject.bio}
-              </p>
-            </div>
-            <div className="part-5 btn flex justify-center items-center gap-2 mt-5">
-              <button
-                onClick={handleClick}
-                className="bg-white w-1/2 py-[10px] flex justify-center items-center rounded-xl"
-              >
-                <AiOutlineClose className="text-3xl  " />
-              </button>
-              <button
-                onClick={handleClick}
-                className="bg-[#FC5149] w-1/2 py-[10px] flex justify-center rounded-xl items-center"
-              >
-                <FaHeart className="text-3xl text-white" />
-              </button>
-            </div>
+          </div>
+          <div className="part overflow-hidden h-fit  bio ">
+            <h2 className="text-xl mt-4 text-white overflow-hidden h-fit">
+              Bio
+            </h2>
+            <p className="text-lg tracking-tight mt-1 overflow-hidden h-fit leading-tight text-white/90">
+              {currentObject.bio}
+            </p>
+          </div>
+          <div className="part-5 overflow-hidden h-fit  btn flex justify-center items-center gap-2 mt-5">
+            <motion.button
+              whileTap={{ scale: 0.9 }}
+              onClick={handleClick}
+              className="bg-white  overflow-hidden h-fit w-1/2 py-[10px] flex justify-center items-center rounded-xl"
+            >
+              <AiOutlineClose className="text-3xl  " />
+            </motion.button>
+            <motion.button
+              whileTap={{ scale: 0.9 }}
+              onClick={handleClick}
+              className="bg-[#FC5149] overflow-hidden h-fit w-1/2  py-[10px] flex justify-center rounded-xl items-center"
+            >
+              <FaHeart className="text-3xl text-white" />
+            </motion.button>
           </div>
         </div>
       </div>
